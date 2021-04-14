8ea5317023ad95a7d6e8c54a42c4eb5a5fc62433
Trayan Iliev <office@iproduct.org>
2021-04-14 12:31:58 +0300

    Book class added inheriting Document

--- my-library/book.py
+++ my-library/book.py
@@ -11,6 +11,15 @@ class Document:
         return f'ID: {self.id}, Title: {self.title}, Subtitle: {self.subtitle}, Authors: {self.authors}, ' \
                f'Tags: {self.tags}, Language: {self.language}'
 
+class Book(Document):
+    def __init__(self, id, title, subtitle, authors, tags, year, language='EN'):
+        Document.__init__(self, id, title, subtitle, authors, tags, language) # call constructor of the base class
+        self.year = year
+    def __str__(self):
+        return f'Book({Document.__str__(self)}, Year: {self.year})'
+
 if __name__ == '__main__':
-    b1 = Document(1, 'Intro to Python', 'Sample presentation', 'T. Iliev', 'python, introduction')
-    print(b1)
+    d1 = Document(1, 'Intro to Python', 'Sample presentation', 'T. Iliev', 'python, introduction')
+    print(d1)
+    b2 = Book(2, 'Learning Python', 'Powerfull Object-Oriented Programming', 'David Ascher, Mark Lutz', 'python, learning, OOP', 1999)
+    print(b2)
