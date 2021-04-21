004a4bbe48382ad028c121f22eb7d1dbe9755651
Trayan Iliev <office@iproduct.org>
2021-04-21 11:32:57 +0300

    formatted output added

--- my-library/library.py
+++ my-library/library.py
@@ -69,9 +69,29 @@ def input_book():
 
     return Book(None, title, subtitle, authors, tags, year, language)
 
+def print_books(books):
+    print('-' * 170)
+    print(f'| {"ID":4} | {"Title":35} | {"Subtitle":35.35} | {"Authors":30} | {"Tags":30} | {"Year":6} | {"Language":8} |')
+    print('-' * 170)
+    for b in books:
+        print(
+            f'| {b.id:4} | {b.title:35.35} | {b.subtitle:35.35} | {b.authors:30.30} | {", ".join(b.tags):30.30} | {b.year:6} | {b.language:8.8} |')
+    print('-' * 170)
+
+
 if __name__ == '__main__':
-    b1 = input_book()
-    print(b1)
+    # b1 = input_book()
+    tags = 'python, learning, OOP'.split(", ")
+    for i in range(len(tags)):
+        tags[i] = tags[i].strip()
+
+    b2 = Book(2, 'Learning Python', 'Powerfull Object-Oriented Programming', 'David Ascher, Mark Lutz',
+              tags, 1999)
+    b3 = Book(3, 'Python for Everybody', 'Powerfull Object-Oriented Programming', 'David Ascher, Mark Lutz',
+              tags, 2020)
+    b4 = Book(4, 'Python by Example', 'Powerfull Object-Oriented Programming', 'Hristo Petrov',
+              tags, 2019, "BG")
+    print_books([b2, b3, b4])
 
 
 
