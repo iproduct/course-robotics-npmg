024832a98ef20bb13814c485f0016cc758d59a28
Trayan Iliev <office@iproduct.org>
2021-11-17 17:00:43 +0200

    examples and presentation added

--- 02-files-lab2/wordcount.py
+++ 02-files-lab2/wordcount.py
@@ -1,3 +1,10 @@
+import nltk
+from nltk.corpus import stopwords
+
+nltk.download('stopwords')
+stops = set(stopwords.words('english'))
+# print(stops)
+# print()
 
 def get_count(wordcount_tupple):
     return wordcount_tupple[1]
@@ -11,9 +18,11 @@ if __name__ == '__main__':
             if len(line) == 0: # if line is emepty
                 continue
             for word in line.split():
+                if len(word) < 2 or word in stops:
+                    continue
                 wordcounts[word] = wordcounts.get(word, 0) + 1
 
     wc_list = [(word, wordcounts[word]) for word in wordcounts]
     wc_list.sort(key = get_count, reverse=True)
-    for wc in wc_list[:20]:
+    for wc in wc_list[:10]:
         print(wc[0], "->", wc[1])
